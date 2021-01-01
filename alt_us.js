a28e5142b29cb3548de345689d99f0062cf5cfc2
Benjamin Webb <40066515+Webb-Ben@users.noreply.github.com>
2021-01-01 10:56:33 -0700

    Add files via upload

---
+++ alt_us.js
@@ -0,0 +1,125 @@
+const width = 1200,
+      height = 750;
+
+const svg = d3.select("body")
+              .append("svg")
+              .attr("viewBox", [0, 0, width, height])
+              .on("dblclick", reset);
+
+const transition = d3.transition();
+const zoom = d3.zoom()
+               .scaleExtent([1, 8]);
+const path = d3.geoPath()
+               .projection(null);
+
+var g = svg.append("g");
+var mask = svg.append("g")
+              .attr("class", mask);
+
+var focus = null;
+
+d3.json("./build/us.json", function(error, us) {
+if (error) return console.error(error);
+    
+    d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv", function(error, data){
+        if (error) return console.error(error);
+           var covid_data = {};
+           if (error) return console.error(error);
+           
+           data.forEach(function(d) {
+                    covid_data[+d.fips] = +d.cases;
+           });
+           
+           var color = d3.scaleQuantize()
+           .domain(Object.values(covid_data))
+//           .range(["#0A2F51", "#0E4D64", "#137177", "#188977", "#1D9A6C", "#39A96B", "#74C67A", "#99D492", "#BFE1B0", "#DEEDCF"]);
+           .range(["#800072", "#8F006C", "#9F0064", "#AF0058", "#BF004A", "#CF0039", "#DF0026", "#EF0011", " #FF0500", "#FD2602", "#FB4605", "#F86507", "#F68209", "#F49F0B", "#F2BA0D", "#EFD510", "#EDED12"]);
+
+
+           
+            
+           const counties = g.append("g")
+            .selectAll("path")
+            .data(topojson.feature(us, us.objects.counties).features)
+            .enter()
+            .append("path")
+           .on("mouseover", onMouseOver)
+           .on("mouseout", onMouseOut)
+           .on("click", clicked)
+            .style("fill", function(d) {
+                 return color(covid_data[+d.id]);
+             })
+            .attr("class", "county")
+            .attr("d", path);
+           
+           g.append("path")
+           .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+           .attr("class", "border border--state")
+           .attr("d", path);
+    });
+
+    mask.append("g")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter()
+    .append("path")
+    .on("mouseover", onMouseOver)
+    .on("mouseout", onMouseOut)
+    .on("click", clicked)
+    .attr("class", "state")
+    .attr("d", path);
+});
+
+    
+    function onMouseOver(event, d) {
+        d3.select(this).style("opacity", 0.95);
+    };
+
+    function onMouseOut(event, d) {
+        d3.select(this).style("opacity", 0.1);
+    };
+
+function reset() {
+   if (focus != null){
+       g
+       .transition()
+       .duration(750)
+       .attr('transform',
+             'translate('+width/2 +','+height/2+')scale('+1+')translate('+-width/2+','+-height/2+')');
+       focus = null;
+       d3.json("./build/us.json", function(error, us) {
+       mask.append("g")
+       .selectAll("path")
+       .data(topojson.feature(us, us.objects.states).features)
+       .enter()
+       .append("path")
+       .on("mouseover", onMouseOver)
+       .on("mouseout", onMouseOut)
+       .on("click", clicked)
+       .attr("class", "state")
+       .attr("d", path);
+               });
+   }
+ }
+
+    function clicked(d) {
+      var x0, y0, x1, y1, k;
+      if (focus != d){
+        [[x0, y0], [x1, y1]] = path.bounds(d);
+        event.stopPropagation();
+        k = Math.min(8, 0.7 / Math.max((x1 - x0) / width, (y1 - y0) / height));
+        focus = d;
+        mask.remove();
+        g.transition().duration(750)
+         .attr('transform',
+               'translate('+width/2 +','+height/2+')scale('+k+')translate('+-(x0+x1)/2+','+-(y0+y1)/2+')');
+                     } else { reset(); };
+     
+    };
+    
+    function zoomed(event) {
+        var {transform} = event;
+        g.attr("transform", transform);
+        g.attr("stroke-width", 1 / transform.k);
+    }
+
--- covid_us.js
+++ covid_us.js
@@ -5,110 +5,111 @@ const svg = d3.select("body")
               .append("svg")
               .attr("viewBox", [0, 0, width, height])
               .on("dblclick", reset),
-      container = d3.select("body")
-                    .append("svg");
-var model_container = {};
+      c = d3.select("body")
+            .append("svg")
+            .attr("viewBox", [0, 0, 0, 0]);
+
+var states = new Object();
+states = build_states();
+console.log(states);
+
+var counties = new Object();
+counties = build_counties();
+console.log(counties);
+//const cvd = covid_nyt_data();
 
 const transition = d3.transition(),
       zoom = d3.zoom(),
       path = d3.geoPath()
                .projection(null);
 
-var graphic = svg.append("g");
+var g = svg.append("g");
 var mask = svg.append("g")
               .attr("class", mask);
 var focus = null;
 
-function build_states(container){
-    d3.json("./build/us.json", function(error, us) {
-    if (error) return console.error(error);
-    
-        container.append("g")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-}
+//var color = d3.scaleQuantize()
+//.domain(Object.values(cvd))
+//.range(["#0A2F51", "#0E4D64", "#137177", "#188977", "#1D9A6C", "#39A96B", "#74C67A", "#99D492", "#BFE1B0", "#DEEDCF"]);
 
-    
-    d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv", function(error, data){
-        if (error) return console.error(error);
-           var covid_data = {};
-           if (error) return console.error(error);
-           
-           data.forEach(function(d) {
-                    covid_data[+d.fips] = +d.cases;
-           });
-           
-           var color = d3.scaleQuantize()
-           .domain(Object.values(covid_data))
-           .range(["#0A2F51", "#0E4D64", "#137177", "#188977", "#1D9A6C", "#39A96B", "#74C67A", "#99D492", "#BFE1B0", "#DEEDCF"]);
-            
-           const counties = g.append("g")
-            .selectAll("path")
-            .data(topojson.feature(us, us.objects.counties).features)
-            .enter()
-            .append("path")
-           .on("mouseover", onMouseOver)
-           .on("mouseout", onMouseOut)
-           .on("click", clicked)
-            .style("fill", function(d) {
-                 return color(covid_data[+d.id]);
-             })
-            .attr("class", "county")
-            .attr("d", path);
-           
-           g.append("path")
-           .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-           .attr("class", "border border--state")
-           .attr("d", path);
+//states
+//.attr("class", "state")
+//.attr("d", path);
+
+console.log( c.selectAll('g'), c.selectAll('.county'), c.selectAll('.state')  );
+
+//g.merge(
+// c.selectAll(".county").clone() )
+//.style("fill", function(d) {
+//     return color(cvd[+d.id]);
+// })
+//.attr("class", "county")
+//.attr("d", path);
+
+
+function build_states(){
+    d3.json("./build/us.json", function(us) {
+    var states = c.append("g").selectAll("path").data(topojson.feature(us, us.objects.states).features).enter().append("path").attr("d", path).attr("class", "state");
+//    if (i) { states.on("mouseover", onMouseOver).on("mouseout", onMouseOut).on("click", clicked)};
+    console.log(states);
+    return d3.values(states);
     });
-    mask.append("g")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.states).features)
-    .enter()
-    .append("path")
-    .on("mouseover", onMouseOver)
-    .on("mouseout", onMouseOut)
-    .on("click", clicked)
-    .attr("class", "state")
-    .attr("d", path);
-});
+};
+            
+//function build_counties(){
+//    d3.json("./build/us.json", function(us) {
+//    var counties = c.append("g").selectAll("path").data(topojson.feature(us, us.objects.counties).features).enter().append("path").attr("d", path).attr("class", "county");
+//    if (i) { counties.on("mouseover", onMouseOver).on("mouseout", onMouseOut).on("click", clicked)};
+//    console.log(counties);
+//    return counties;
+//    });
+//};
+            
+//function covid_nyt_data(){
+//    var cv_data = {};
+//    d3.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv", function(error, data){
+//    data.forEach(function(d) {
+//                 cv_data[+d.fips] = +d.cases;
+//                 });
+//    });
+//    return cv_data;
+//};
 
+//function onMouseOver(event, d) {
+//    d3.select(this).style("opacity", 0.8);
+//};
+
+//function onMouseOut(event, d) {
+//    d3.select(this).style("opacity", 0.3);
+//};
+
+//function reset() {
+//   if (focus != null){
+//       g
+//       .transition()
+//       .duration(750)
+//       .attr('transform',
+//             'translate('+width/2 +','+height/2+')scale('+1+')translate('+-width/2+','+-height/2+')');
+//       focus = null;
+//   };
+// };
+
+//    function clicked(d) {
+//      var x0, y0, x1, y1, k;
+//      if (focus != d){
+//        [[x0, y0], [x1, y1]] = path.bounds(d);
+//        event.stopPropagation();
+//        k = Math.min(8, 0.7 / Math.max((x1 - x0) / width, (y1 - y0) / height));
+//        focus = d;
+//        mask.remove();
+//        g.transition().duration(750)
+//         .attr('transform',
+//               'translate('+width/2 +','+height/2+')scale('+k+')translate('+-(x0+x1)/2+','+-(y0+y1)/2+')');
+//                     } else { reset(); };
+//    };
     
-    function onMouseOver(event, d) {
-        d3.select(this).style("opacity", 0.95);
-    };
-
-    function onMouseOut(event, d) {
-        d3.select(this).style("opacity", 0.1);
-    };
-
-function reset() {
-   if (focus != null){
-       g
-       .transition()
-       .duration(750)
-       .attr('transform',
-             'translate('+width/2 +','+height/2+')scale('+1+')translate('+-width/2+','+-height/2+')');
-       focus = null;
-   }
- }
-
-    function clicked(d) {
-      var x0, y0, x1, y1, k;
-      if (focus != d){
-        [[x0, y0], [x1, y1]] = path.bounds(d);
-        event.stopPropagation();
-        k = Math.min(8, 0.7 / Math.max((x1 - x0) / width, (y1 - y0) / height));
-        focus = d;
-        mask.remove();
-        g.transition().duration(750)
-         .attr('transform',
-               'translate('+width/2 +','+height/2+')scale('+k+')translate('+-(x0+x1)/2+','+-(y0+y1)/2+')');
-                     } else { reset(); };
-    };
-    
-    function zoomed(event) {
-        var {transform} = event;
-        g.attr("transform", transform);
-        g.attr("stroke-width", 1 / transform.k);
-    }
+//    function zoomed(event) {
+//        var {transform} = event;
+//        g.attr("transform", transform);
+//        g.attr("stroke-width", 1 / transform.k);
+//    };
